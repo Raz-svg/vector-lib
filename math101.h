f1e3557dd2be256f2c4eedd5618edf8fcd5fcf22
aksh_raz <sur37704@gmail.com>
2025-06-02 11:38:03 +0530

    aabb

--- math101.h
+++ math101.h
@@ -4,7 +4,6 @@ typedef struct{
   float x,y;
 }point;
 
-// point constructor
 point p(float x,float y){
 
   point p;
@@ -14,18 +13,13 @@ point p(float x,float y){
 }
 
 // point addition
-point padd(point a,point b){
-  point result={a.x+b.x,a.y+b.y};
-  return result;
-}
-
+point padd(point a,point b){point result={a.x+b.x,a.y+b.y};return result;}
 
 //. Vector
 typedef struct{
   float x,y; 
 } vector;
 
-// vector constructor
 vector v2(float x,float y){
 
   vector p;
@@ -35,24 +29,25 @@ vector v2(float x,float y){
 }
 
 // vector addition
-vector add(vector a,vector b){
-  vector result = {a.x+b.x,a.y+b.y};
-  return result;
-}
-
-
+vector add(vector a,vector b){vector result = {a.x+b.x,a.y+b.y};return result;}
 // vector subtraction
-vector sub(point a,point b){
-  vector result ={a.x-b.x,a.y-b.y};
-  return result;
-}
-
-
+vector sub(point a,point b){vector result ={a.x-b.x,a.y-b.y}; return result;}
 // Vector+Point =Point translated int the direction of the vector 
-point vaddp(vector v,point p){
-  point result ={v.x+p.x,v.y+p.y};
-  return result;
-}
+point vaddp(vector v,point p){ point result ={v.x+p.x,v.y+p.y} ;  return result;}
+vector smulv(vector v,float a){vector result = {v.x*a,v.y*a};return result;}
+
+
+typedef struct{
+  vector max;
+  vector min;
+}aabb;
+
+
+float width(aabb box){return box.max.x-box.min.x;}
+float height(aabb box){return box.max.y-box.min.y;}
+
+vector mid(aabb box){return smulv((add(box.min,box.max)),0.5f);}
+
 
 
 
